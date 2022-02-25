c56eb7f7c3c13ccba5c6832204a0c0ba539d5ba6
Berthuuu <quynhdn@yahoo.com>
2022-02-25 02:40:12 -0800

    Unit 2 Lesson A done!
    Finished lesson A

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
+    private float xRange = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
     }
 }
